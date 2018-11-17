29cf15b3695130766869c45ca1d80d172e8cc5c6
Omar Peracha <omarperacha@Omars-MBP.lan>
2018-11-17 22:53:01 +0000

    fix typos in dataset

--- utils.py
+++ utils.py
@@ -1,1 +1,16 @@
-import os
+import glob
+import csv
+import numpy as np
+
+vectorisedSamples = glob.glob("vectorised dataset/*.csv")
+
+allData = np.array([], dtype='str')
+
+
+for i in vectorisedSamples:
+    data = np.genfromtxt(i, delimiter=',', dtype='str')
+    print(i, ": ", len(data))
+    allData = np.append(allData, data)
+    
+print(np.shape(allData))
+print(np.unique(allData))
