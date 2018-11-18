c6c45fb4ee4ced66ae07f83cd8ea33eb0edbaca9
Omar Peracha <omarperacha@Omars-MBP.lan>
2018-11-18 19:27:45 +0000

    to categorical

--- train.py
+++ train.py
@@ -3,3 +3,9 @@ Created on 18 Nov 2018
 
 @author: omarperacha
 '''
+from utils import getData
+import numpy as np
+
+x_train = getData()
+
+print(np.shape(x_train))
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import glob
 import csv
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
 def getData():
@@ -18,6 +19,17 @@ def getData():
         allData[count, 0:] = npData
         count += 1
         
-    print(np.shape(allData))
-    print(np.unique(allData))
+    allData = toCategorical(allData)
+    
     return(allData)
+
+
+def toCategorical(myArray):
+    myArray = np.reshape(myArray, (5760))
+    encoder = LabelEncoder()
+    transfomed_label = encoder.fit_transform(myArray)
+    transfomed_label = np.reshape(transfomed_label, (10, 576))
+    return(transfomed_label)
+
+
+
